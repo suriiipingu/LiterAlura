9508927bde5233ae0dc4729079a500c3684711d8
suriiipingu <btsherwatching@gmail.com>
2025-01-16 19:49:26 -0400

    Initial commit

---
+++ src/main/java/br/com/one/LiterAlura/LiterAluraApplication.java
@@ -0,0 +1,18 @@
+package br.com.one.LiterAlura;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("teste");
+	}
+}
